fa233ec653c5056d46c28c13eebf64d5c4988d69
Matheus Mandotti <mathew.mfranco@gmail.com>
2018-03-26 12:55:47 -0300

    Crição de uma lib Conta.jar e aula5 com nada incluso

--- pratica3/aula5/Conta/src/linguagens1/Cliente.java
+++ pratica3/aula5/Conta/src/linguagens1/Cliente.java
@@ -1,9 +1,41 @@
-package aula4;
+package linguagens1;
 
 public class Cliente {
-    String nome;
-    String sobrenome;
-    String cpf;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    
+    
+    //construtor
+    public Cliente(String nome,String sobrenome, String cpf){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+  
+    }
+    
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void mudarCPF(String cpf){
+        if(validarCPF(cpf)){
+            this.cpf = cpf;
+        } else {
+            System.out.println("Não foi possivel alterar o CPF.");
+            }
+        }
+    
+    
+    private boolean validarCPF(String cpf){
+        
+        
+        
+        return true; 
+    }
+    
+ 
    
     
 }
---
+++ pratica3/aula5/Conta/src/linguagens1/Conta.java
@@ -0,0 +1,74 @@
+package linguagens1;
+
+public class Conta {
+    
+    String numeroConta; 
+    private double saldo;
+    Cliente cliente;
+    
+    //construtor
+    public Conta (String nConta, double saldo, String nome, String sobrenome, String cpf ){
+        
+        this.saldo = saldo;
+        this.numeroConta = nConta;
+        this.cliente = new Cliente(nome, sobrenome, cpf);
+ 
+    }
+    
+ 
+    public String visualizarSaldo(){
+  
+        return Double.toString(this.saldo);
+        //devolve string
+        
+    }
+    
+    public double getSaldo() {
+        
+        return this.saldo;
+        //retorna um double
+      
+    }
+    
+    public boolean depositar(double valor){
+        if(valor <= 0){
+            System.out.println("Nada a depositar");
+            return false;
+        } else {
+            this.saldo += valor;
+            return true;
+        }
+        
+    }
+    
+    public boolean sacar(float valor){
+        boolean resp = false;
+        if(this.saldo >= valor) {
+           
+            
+            this.saldo = this.saldo - valor;
+            resp = true;
+        }
+        return resp;
+    }
+ 
+
+    public boolean transferirDinheiro(Conta destinatario, float valor){
+        boolean resp = false;
+        if(this.sacar(valor)){  
+            destinatario.depositar(valor);
+            resp = true;
+        }
+        return resp;
+    }
+    
+    
+    
+    
+    public String getnumeroConta (){
+        
+        return this.numeroConta;
+    
+    }
+
+}
