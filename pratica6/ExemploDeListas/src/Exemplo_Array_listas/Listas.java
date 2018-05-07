40e4f61d88e250e792cf475771e7c130eef5d268
Matheus Mandotti <mathew.mfranco@gmail.com>
2018-05-07 12:43:39 -0300

    Exemplo de ArrayList

---
+++ pratica6/ExemploDeListas/src/Exemplo_Array_listas/Listas.java
@@ -0,0 +1,29 @@
+package Exemplo_Array_listas;
+
+import java.util.ArrayList;
+
+public class Listas {
+
+    public static void main(String[] args) {
+        
+       //dentro do <> passa-se o tipo, a classe do arrayList 
+       ArrayList<String> arrList = new ArrayList<String>();
+       
+       arrList.add("Vai 1");
+       arrList.add("Vai 2");
+       arrList.add("Vai 3");
+       
+       // ArrayList<item> ListaC  = new ArrayList<item>;
+       //ListaC.add(new item ("",preco);
+       //ou listaC.add(item,preco);
+       
+       // a 
+      
+       Integer a = 22;
+       
+       System.out.println(arrList);
+
+        
+    }
+    
+}
